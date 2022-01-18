28615d34e4c0a69d5ca34e318a0ce0ce57810b60
habadong <44386449+habadong@users.noreply.github.com>
2022-01-18 23:39:37 +0900

    welcome bot
    환영인사 봇

---
+++ generateImage.js
@@ -0,0 +1,65 @@
+const Canvas = require("canvas")
+const Discord = require("discord.js")
+const background = "https://images.unsplash.com/photo-1557682250-33bd709cbe85?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1429&q=80"
+
+const dim = {
+    height: 675,
+    width: 1200,
+    margin: 50
+}
+
+const av = {
+    size: 256,
+    x: 480,
+    y: 170
+}
+
+const generateImage = async (member) => {
+    let username = member.user.username
+    let discrim = member.user.discriminator
+    let avatarURL = member.user.displayAvatarURL({format: "png", dynamic: false, size: av.size})
+
+    const canvas = Canvas.createCanvas(dim.width, dim.height)
+    const ctx = canvas.getContext("2d")
+
+    // draw in the background
+    const backimg = await Canvas.loadImage(background)
+    ctx.drawImage(backimg, 0, 0)
+
+    // draw black tinted box
+    ctx.fillStyle = "rgba(0,0,0,0.2)"
+    ctx.fillRect(dim.margin, dim.margin, dim.width - 2 * dim.margin, dim.height - 2 * dim.margin)
+
+    const avimg = await Canvas.loadImage(avatarURL)
+    ctx.save()
+
+    ctx.beginPath()
+    ctx.arc(av.x + av.size / 2, av.y + av.size / 2, av.size / 2, 0, Math.PI * 2, true)
+    ctx.closePath()
+    ctx.clip()
+
+    ctx.drawImage(avimg, av.x, av.y)
+    ctx.restore()
+
+    // write in text
+    ctx.fillStyle = "white"
+    ctx.textAlign = "center"
+
+    // draw in welcome
+    ctx.font = "50px Roboto"
+    ctx.fillText("환영합니다", dim.width/2, dim.margin + 70)
+
+    // draw in the username
+    ctx.font = "60px Roboto"
+    ctx.fillText(username, dim.width/2, dim.height - dim.margin - 125)
+
+    // draw in to the server
+    ctx.font = "40px Roboto"
+    ctx.fillText("님이 입장했습니다", dim.width / 2, dim.height - dim.margin - 50)
+
+    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "welcome.png")
+    return attachment
+
+}
+
+module.exports = generateImage
